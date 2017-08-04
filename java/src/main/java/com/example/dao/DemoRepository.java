cacfd5e32636fca4beb451901e329875deb00fae
785172550@qq.com <785172550@qq.com>
2017-08-04 17:19:23 +0800

    add the demo repository
    add the demo repository

---
+++ java/src/main/java/com/example/dao/DemoRepository.java
@@ -0,0 +1,11 @@
+package com.example.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by Administrator on 2017/8/4.
+ *
+ */
+public interface DemoRepository extends JpaRepository<String,Long>{
+
+}
