820881a506e799f3e901fc03daab396a0fb27466
785172550@qq.com <785172550@qq.com>
2017-08-05 18:24:42 +0800

    添加的热部署和异常
    添加的热部署和异常

--- java/src/main/java/com/example/controller/DemoController.java
+++ java/src/main/java/com/example/controller/DemoController.java
@@ -25,7 +25,7 @@ public class DemoController {
 //        return new Result.Builder<String>().setMsg("ok").setContent("hello world").build();
         String tes;
         Map<String, String> res = new HashMap();
-        res.put("test key", "test content ");
+        res.put("test key", "test content");
         return res;
     }
 
