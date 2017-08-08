ff58dadd73c773b6749c0741b2281d5741aef173
785172550@qq.com <785172550@qq.com>
2017-08-08 15:26:50 +0800

    unit test
    unit test

--- java/src/main/java/com/example/controller/StockController.java
+++ java/src/main/java/com/example/controller/StockController.java
@@ -1,10 +1,14 @@
 package com.example.controller;
 
 import com.example.model.Stock;
+import com.example.utils.DemoUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Kenneth on 2017/8/8.
@@ -23,4 +27,17 @@ public class StockController {
         return stock;
     }
 
+    @RequestMapping("/stock_10")
+    public List<Stock> getTop10() {
+        List<Stock> res = new ArrayList<>();
+        res = DemoUtils.GenerateStock(10);
+        return res;
+    }
+
+    @RequestMapping("/stock_special")
+    public List<Stock> getSpecial() {
+        List<Stock> res = new ArrayList<>();
+        res = DemoUtils.GenerateStock(9);
+        return res;
+    }
 }
---
+++ java/src/test/java/com/example/algor/AlgorTest.java
@@ -0,0 +1,20 @@
+package com.example.algor;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Kenneth on 2017/8/8.
+ *
+ */
+public class AlgorTest {
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void AlgorTest(){
+
+    }
+}
