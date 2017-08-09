7a514c6276cf79556301153a1f8689910ed01a84
785172550@qq.com <785172550@qq.com>
2017-08-09 10:57:29 +0800

    almost complete
    almost complete

--- java/src/main/java/com/example/dao/DemoRepository.java
+++ java/src/main/java/com/example/dao/DemoRepository.java
@@ -16,6 +16,9 @@ public interface DemoRepository extends JpaRepository<Stock,Integer> {
     @Query(value = "select * from stock s where s.stockname = ?1",nativeQuery = true)
     List<Stock> getStockByName(@Param("stockname") String name);
 
-    @Query(value = "select * from stock s where s.date between ?1 and ?2",nativeQuery = true)
-    List<Nasdaq_2013> getStockByDate(int start,int end);
+    @Query(value = "select * from stock s where s.date between ?1 and ?2 and time = 0",nativeQuery = true)
+    List<Stock> getStockByDateNoTime(int start,int end);
+
+    @Query(value = "select * from stock s where s.date between ?1 and ?2 and time <> 0",nativeQuery = true)
+    List<Stock> getStockDate(int start,int end);
 }
--- java/src/main/java/com/example/service/DemoService.java
+++ java/src/main/java/com/example/service/DemoService.java
@@ -5,4 +5,8 @@ package com.example.service;
  *
  */
 public class DemoService {
+
+    public void getSomething(){
+
+    }
 }
